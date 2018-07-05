2f9465ec0b6a452bfc04a65b9a758f05ddf4dc82
enisyavas <enisyavas@gmail.com>
2018-07-05 23:04:36 +0300

    First Commit

---
+++ Email/Entities/Email.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Email.Entities
+{
+    public abstract class Email
+    {
+        private string _id;
+        public string id
+        {
+            get { return _id; }
+            set { _id = value; }
+
+        }
+
+        private long _date;
+        public long date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+    }
+}
---
+++ Email/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Email.Entities
+{
+    public abstract class User
+    {
+        private string _id;
+        public string id
+        {
+            get { return _id; }
+            set { _id = value; }
+
+        }
+    }
+}
